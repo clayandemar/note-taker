e7fc06a90a9bd22f65d0475186a657c7b12dcf41
clayandemar <clayandemar@gmail.com>
2023-02-02 19:17:26 -0600

    notes
    saving notes written

--- Develop/server.js
+++ Develop/server.js
@@ -5,33 +5,36 @@ const fs = require('fs');
 const PORT = 3001;
 const app = express();
 
+var savedNotes = []
+
 //Parses JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//Message showing that localhost is running
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+})
+
 //Route to Home Page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+)
 
 //Route to Notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+)
 
-// ----
+//Save button
 app.post('/api/notes', (req, res) =>
-console.log(req)
-);
+{
+  savedNotes.push(req.body)
+  return console.log(req.body);
+  
 
+  // res.send('{ hello }')
+}
+)
 
-
-
-
-
-
-//Message showing that localhost is running 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
