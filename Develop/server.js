fdfafa347389f19050c4d932de7b34de68c12fd1
clayandemar <clayandemar@gmail.com>
2023-02-02 18:29:54 -0600

    routes
    no more routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const PORT = 3001;
 const app = express();
-var notes = JSON.parse(data);
 
 //Parses JSON
 app.use(express.json());
@@ -21,26 +20,18 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+// ----
+app.post('/api/notes', (req, res) =>
+console.log(req)
+);
+
+
+
+
+
+
+
 //Message showing that localhost is running 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-//Save notes written
-
-//Route specific notes
-app.post('/api/notes', function(req, res){
-  let newNote = req.body;
-  notes.push(newNote);
-  updateDb();
-  return console.log("Added new note: "+newNote.title);
-});
-
-//Route Specific ID for note
-app.get("/api/notes/:id", function(req,res) {
-  res.json(notes[req.params.id]);
-});
-
-
-
-
